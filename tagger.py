7a2304eb02c9dc01f25a77feac41719d84a4bad0
David <davferrufino11@gmail.com>
2023-02-23 16:16:15 -0500

    Initial commit

---
+++ tagger.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import sys
+
+train = sys.argv[1]
+
+testSet = sys.argv[2]
+
+taggedSet = sys.argv[4]
+
+#Tag regex: /\/[A-Z#$.,:()"']*/gm
+{}
