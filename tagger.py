67e6ad6b9e1b53759c1f3753983f45bc34257aca
deHank <davferrufino11@gmail.com>
2023-02-27 14:46:14 -0500

    - Words are counted and put into ditonary called WordCount

--- tagger.py
+++ tagger.py
@@ -16,12 +16,28 @@ trainData = ''
 with open(train, "r") as f:
     trainData += f.read()
 
-#Getting all of the words in the corpus
+# Getting all of the words in the corpus
 res = re.findall(r'\S+', trainData)
 
-tags = re.findall(r'/\/[\S]+/gm')
+#tags = re.findall(r'/\/[\S]+/gm')
 
+# Now I need to create a dictionary with 1 size tuples
+# Make a dictionary with tags as key, and total count of tags
 
+tagCount = {}
+wordCount = {}
+for j in range(len(res)):
+    #Splitting word before tag, so if it was Pierre/NNP, i'd get Pierre
+    word = res[j].partition("/")[0]
+    # for i in range(len(res) - j + 1):
+    # Generate our n-grams, this will generate n-grams up to the size of the ngram parameter
+    tagCount = tuple(word)
 
-#Tag regex: /\/[A-Z#$.,:()"']*/gm
+    # Counting ngram occurence
+    if word in wordCount:
+        wordCount[word] += 1
+    else:
+        wordCount[word] = 1
+
+# Tag regex: /\/[A-Z#$.,:()"']*/gm
 {}
